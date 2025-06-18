a811e98a5cec9ab660f0a9b69cc06708f6021fb7
Harm Roukema <harmroukema@gmail.com>
2025-06-18 14:08:37 +0200

    Added automatic submodule download

--- modules/50-vulnscan/install.sh
+++ modules/50-vulnscan/install.sh
@@ -6,4 +6,5 @@ apt install -y firefox-esr
 uv tool install wappalyzer
 uv pip install -r "$SCRIPT_DIR/requirements.txt"
 
+git submodule update --init --recursive
 bash "$SCRIPT_DIR/search_vulns/install.sh"
